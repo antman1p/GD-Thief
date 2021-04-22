86d82dfa7d9f7515dc709d90463e4a8f44d3246b
Antonio Piazza <AntonioPiazza@c02dk08gmd6w.attlocal.net>
2021-04-22 14:58:48 -0500

    Added .gitignore

--- gd_thief.py
+++ gd_thief.py
@@ -19,18 +19,18 @@ def build_service():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('./credentials/token.json'):
+        creds = Credentials.from_authorized_user_file('./credentials/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                './credentials/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open('./credentials/token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('drive', 'v3', credentials=creds)
